ab6f942189e133eedd907da5208f2f5b88745755
Vinícius <viniciusmartines.estudos@gmail.com>
2026-03-04 16:47:45 -0300

    teste de workflow

---
+++ plus.py
@@ -0,0 +1,17 @@
+import sys
+
+def plus(a : int, b : int) -> int:
+    assert isinstance(a, int), "a must be an integer"
+    assert isinstance(b, int), "b must be an integer"
+    return a + b
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("É necessário fornecer dois números inteiros como argumentos.")
+        sys.exit(1)
+    
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    
+    result = plus(a, b)
+    print(f"A soma de {a} e {b} é {result}.")
